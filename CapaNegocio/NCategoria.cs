50583a92be44e13e374b616f2868ba19f1de3850
David Garcia <davidfdo.garcia@gmail.com>
2022-11-07 17:23:24 -0600

    cambios

--- CapaNegocio/NCategoria.cs
+++ CapaNegocio/NCategoria.cs
@@ -15,6 +15,52 @@ namespace CapaNegocio
         //Método insertar que llama al método insertar de la clase DCategoria
         // de la CapaDatos
 
+        public static string Insertar(string nombre, string descripcion)
+        {
+            DCategoria Obj = new DCategoria();
+            Obj.Nombre = nombre;
+            Obj.Descripcion = descripcion;
+            return Obj.Insertar(Obj);
+        }
 
+        //Método editar que llama al método editar de la clase DCategoria
+        // de la CapaDatos
+
+        public static string Editar(int idcategoria, string nombre, string descripcion)
+        {
+            DCategoria Obj = new DCategoria();
+            Obj.Idcategoria = idcategoria;
+            Obj.Nombre = nombre;
+            Obj.Descripcion = descripcion;
+            return Obj.Editar(Obj);
+        }
+
+        //Método eliminar que llama al método eliminar de la clase DCategoria
+        // de la CapaDatos
+
+        public static string Eliminar(int idcategoria)
+        {
+            DCategoria Obj = new DCategoria();
+            Obj.Idcategoria = idcategoria;
+            return Obj.Eliminar(Obj);
+        }
+
+        //Método mostrar que llama al método mostrar de la clase DCategoria
+        // de la CapaDatos
+
+        public static DataTable Mostrar()
+        {
+            return new DCategoria().Mostrar(); 
+        }
+
+        //Método buscarnombre que llama al método buscarnombre de la clase DCategoria
+        // de la CapaDatos
+
+        public static DataTable BuscarNombre(string textobuscar)
+        {
+            DCategoria Obj = new DCategoria();
+            Obj.Textobuscar = textobuscar;
+            return Obj.BuscarNombre(Obj);
+        }
     }
 }
