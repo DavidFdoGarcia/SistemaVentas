667e8cc8b83d8e9fc2976f7f1eb04bedf2074fe7
DavidFdoGarcia <73084758+DavidFdoGarcia@users.noreply.github.com>
2022-11-07 17:01:03 -0600

    Add files via upload

---
+++ CapaNegocio/NCategoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace CapaNegocio
+{
+    class NCategoria
+    {
+        //Método insertar que llama al método insertar de la clase DCategoria
+        // de la CapaDatos
+
+
+    }
+}
---
+++ CapaPresentacion/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaPresentacion
+{
+    public class Class1
+    {
+        public static string cn = "Data Source=192.168.1.151;Initial Catalog=dbventas;Persist Security Info=True;User ID=dttdato;Password=123456";
+    }
+}
